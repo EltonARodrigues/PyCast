7c88d201edee38ce1bd75de7dd82b1827de686a8
EltonARodrigues <eltonrodrigues61@gmail.com>
2016-02-23 17:31:56 -0500

    Pequena limpeza no feed_teste2def e começando a trabalhar com banco de dados

--- feed_teste2def.py
+++ feed_teste2def.py
@@ -45,61 +45,69 @@ def limpar_link(mp3):
 
 ###############################################################################
 
-contador = 0
+
 t=0
 lista = {}
-url = 'feeds.feedburner.com/hack-n-cast'
-#url = input("Insira link do site: ")
-d, nprogramas = feed_in(url)
-pesquisa  = input ("Nome do EP: ")
-pesquisa = str.title(pesquisa)
 lista_de_escolha = []
 numeros = 0
+
+url = 'feeds.feedburner.com/hack-n-cast'
+#url = input("Insira link do site: ")
+
+d, nprogramas = feed_in(url)
+
+pesquisa  = input ("Nome do EP: ")
+pesquisa = str.title(pesquisa)
+
+
 print ('- ID - ')
-for i in range(nprogramas):
+try:
+    for i in range(nprogramas):
 
-    #print(d['entries'][i]['title'])
-    titulo = (d['entries'][i]['title'])
-    #mp3 = str(d['entries'][40]['media_content'])
-    if str.find(titulo,pesquisa) != -1:
-        contador = contador + 1
-        valor_pesquisa = i
-        lista[int(t)] = i
-        t = t + 1
-        lista_de_escolha += [i]
-        numeros = numeros + 1
-        print ('"',i,'" | ',d['entries'][i]['title'])
-################################################################
-'''
-print (lista_de_escolha)
-print (numeros)
-print (lista_de_escolha[numeros-1])'''
-################################################################
-if numeros > 1:
-    escolha = 0
-    escolha = input("Escolha um episódio pelo ID: ")
-    escolha = int(escolha)
-    valor_pesquisa = 'vazio'
-    for c in range(0,numeros):
-        if lista_de_escolha[c]==escolha:
-            valor_pesquisa = escolha
-    if valor_pesquisa == 'vazio':
-        print ('Escolha não existe')
-        exit()
+        titulo = (d['entries'][i]['title'])
 
-mp3 = str(d['entries'][valor_pesquisa]['media_content'])
-nome = d['entries'][valor_pesquisa]['title']
-print('Link: '+limpar_link(mp3))
-print (nome)
-dow = input("Deseja realizar o download desse podcast(sim/nao): ")
-dow = str.upper(dow)
+        if str.find(titulo,pesquisa) != -1:
 
-if dow == "SIM":
+            valor_pesquisa = i
+            lista[int(t)] = i
+            t += 1
+            lista_de_escolha += [i]
+            numeros +=  1
+            print ('"',i,'" | ',d['entries'][i]['title'])
 
-    print ("Realizando Download...")
-    download(nome,limpar_link(mp3))
 
-print("Saindo....")
+    if numeros > 1:
 
-#except NameError:
-#    print("Nome não encontrado...realizar pesquisa novamentes")
+        escolha = 0
+        escolha = input("Escolha um episódio pelo ID: ")
+        escolha = int(escolha)
+        valor_pesquisa = 'vazio'
+
+        for c in range(0,numeros):
+
+            if lista_de_escolha[c]==escolha:
+                valor_pesquisa = escolha
+
+        if valor_pesquisa == 'vazio':
+            print ('Escolha não existe')
+            exit()
+
+
+    mp3 = str(d['entries'][valor_pesquisa]['media_content'])
+    nome = d['entries'][valor_pesquisa]['title']
+
+
+    print (nome)
+
+    dow = input("Deseja realizar o download desse podcast(sim/nao): ")
+    dow = str.upper(dow)
+
+    if dow == "SIM":
+
+        print ("Realizando Download...")
+        download(nome,limpar_link(mp3))
+
+    print("Saindo....")
+
+except NameError:
+    print("Nome não encontrado...realizar pesquisa novamentes")
