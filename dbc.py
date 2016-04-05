ed3577c22ec133c02dc0fce28726e568eb1e14c3
EltonARodrigues <eltonrodrigues61@gmail.com>
2016-04-05 17:38:27 -0300

    Função Basica do BD funcionando

--- dbc.py
+++ dbc.py
@@ -5,6 +5,7 @@ import sqlite3
 
 class DBconnect(object):
 
+    
     def insert_feed(self,p_nome,p_url,p_link):
         print(p_nome)
         print(p_url)
@@ -23,29 +24,38 @@ class DBconnect(object):
                 """,(p_nome,p_url,p_link))
         conn.commit()
         conn.close()
-    def select_feed(self):
 
+    def select_feed(self):
+        cont = 0 
         
         conn = sqlite3.connect('feed_list.db')
         cursor = conn.cursor()
 
         # lendo os dados
-        cursor.execute("""
-                SELECT * FROM feed ORDER BY id;
-                """)
+        cursor.execute("SELECT * FROM feed ORDER BY id;")
         
         print('Tabelas:')
         print("\tID\tPodCast\t\t\tLink Site\t\tURL Feed")
         print("-"*89)
         for linha in cursor:
             print('| \t{}\t{}\t{}\t{}\t|'.format(linha[0],linha[1],linha[2],linha[3]))
-            '''
-            print('-'*10)
-            print('ID: %s' % (linha[0],))
-            print('Nome: %s' % (linha[1],))
-            print( 'Tel: %s' % (linha[2],))
-             '''
-        
-            
-                     
+            cont = cont + 1
+           
         conn.close()
+        return cont - 1
+
+    def select_feedpod(self,id):
+        cont = 0 
+        
+        conn = sqlite3.connect('feed_list.db')
+        cursor = conn.cursor()
+
+        # lendo os dados
+        cursor.execute('SELECT link FROM feed WHERE id = ?', (id,))
+        link_pesq = cursor.fetchall()[0][0]
+
+        #print(cursor.fetchall())
+        #for url in cursor.fetchall():
+
+        conn.close()
+        return link_pesq
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import feedparser
 import urllib
 import urllib.request
 from modulos.modulos_main import Modulos
+from dbc import DBconnect
+import os 
 #import pdb; pdb.set_trace()
 
 if __name__ == '__main__':
@@ -19,16 +21,33 @@ if __name__ == '__main__':
             """)
 
     m = Modulos()
+    db = DBconnect()
 
-    url = input("Insira URL do feed: ")
-    #url = 'http://feeds.feedburner.com/jack-animeclub'
-    #url = "http://feed.nerdcast.com.br"
+    id_p = 'n'
+    while id_p == 'n':
+        cont_p = db.select_feed()    
+        id_p = input("Digite o ID ou (n) para novo podcast: ")
+        print(cont_p)
+        if id_p == 'n' or id_p == 'N':
+            
+            url = 'http://feeds.feedburner.com/jack-animeclub'
+            
+            d = feedparser.parse(url)
+            link_p = d.feed.title_detail.base
+            title_p = d.feed.title_detail.value
+            url_site =  d.feed.link
+            db.insert_feed(title_p,url_site,link_p)        
+            os.system('clear')
+
+        else:
+            url = db.select_feedpod(id_p)   
+
 
     pesquisa  = input ("Nome do EP: ")
     pesquisa = str.title(pesquisa)
 
     nome_mp3, mp3 = m.pesquisa_pod(m.feed_in(url),pesquisa)
-    #print ("\n{}\n".forma(nome_mp3))
+    print ("\n{}\n".format(nome_mp3))
 
     dow = input("Deseja realizar o download desse podcast(sim/nao): ")
     dow = str.upper(dow)
